b00a7dcdc4799e77e3e07c0242ca8c0dcb3c7749
Azizasarbasova06 <azizasarbas@gmail.com>
2026-01-30 15:46:52 +0500

    Add statements

--- Practice1/python-basics/statements.py
+++ Practice1/python-basics/statements.py
@@ -2,18 +2,21 @@
 print("Python is fun!")
 
 #2
-"""
+
 print("Hello World!")
 print("Have a good day.")
 print("Learning Python is fun!") 
-"""
+
 
 #3 
-"""
+
 print("Hello"); print("How are you?"); print("Bye bye!")
-"""
+
 
 #4
-""" print("Python is fun!"); print("Really!") """
+print("Python is fun!"); print("Really!") 
 
 #5
+x = 10
+if x > 5:
+    print("x greater than 5")
